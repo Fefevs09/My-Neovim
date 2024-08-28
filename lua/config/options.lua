bfd97f5a8225334be0299496d9be1ce04bfae851
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-08-27 22:39:14 -0300

    feat: add initial configs

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.autoformat = true
+vim.opt.smartindent = true
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
---
+++ lua/plugins/project.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      manual_mode = true,
+    },
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("projects")
+      end)
+    end,
+  },
+  {
+
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    keys = {
+      { "<leader>fp", pick, desc = "Projects" },
+    },
+  },
+}
