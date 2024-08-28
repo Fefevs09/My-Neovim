71feffa4929a19ba0136ba6d99174c3676594728
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-08-27 22:59:56 -0300

    feat: add markdown and project plugins extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.astro" },
-
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
   },
