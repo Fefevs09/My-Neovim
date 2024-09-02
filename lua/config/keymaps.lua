0015bfafdd4375a1be9992fec6540426d171c820
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-09-01 23:04:30 -0300

    feat: add config for neovide

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,16 @@ vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 -- Toogle Term
 vim.api.nvim_set_keymap("n", "<C-/>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-+>",
+  ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+  { silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-->",
+  ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+  { silent = true }
+)
+vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,10 @@ vim.opt.smartindent = true
 vim.opt.wrap = false -- No Wrap lines
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
+
+-- neovide options
+if vim.g.neovdie then
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  vim.g.neovide_padding_top = 10
+end
