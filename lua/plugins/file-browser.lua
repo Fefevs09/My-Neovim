0cad85e571d6f0b4137abeb507a238b22f55e54e
Felipe Vieira <felipefernandes.contato09@gmail.com>
2024-09-07 00:13:56 -0300

    feat: add `telescope-file-browser`

---
+++ lua/plugins/file-browser.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  config = function()
+    local telescope = require("telescope")
+    local fb_actions = require("telescope._extensions.file_browser.actions")
+
+    telescope.setup({
+      extensions = {
+        file_browser = {
+          theme = "ivy",
+          hijack_netrw = true,
+          mappings = {
+            ["i"] = {
+              ["<C-w>"] = function()
+                vim.cmd("normal vbd")
+              end,
+            },
+            ["n"] = {
+              ["n"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["/"] = function()
+                vim.cmd("startinsert")
+              end,
+            },
+          },
+        },
+      },
+    })
+
+    telescope.load_extension("file_browser")
+
+    vim.api.nvim_set_keymap(
+      "n",
+      "<space>.",
+      ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      { noremap = true }
+    )
+  end,
+}
